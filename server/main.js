29d68cc76fac06ddcfcfac8c95c39a8d53f6eba3
VoXzE <sinncere520@gmail.com>
2019-09-05 16:43:48 -0400

    attempt to fix ESX missing byte thing

--- server/main.js
+++ server/main.js
@@ -1,7 +1,11 @@
 let ESX = null;
 
-emit("esx:getSharedObject", (obj) => ESX = obj);
-
-ESX.RegisterUsableItem("tunerchip", (source) => {
-  emitNet("xgc-tuner:openTuner", source)
-});
+let checkESX = setInterval(() => {
+  if (ESX === null) {
+    emit("esx:getSharedObject", (obj) => ESX = obj);
+    ESX.RegisterUsableItem("tunerchip", (source) => {
+      emitNet("xgc-tuner:openTuner", source)
+    });
+    clearInterval(checkESX);
+  }
+}, 500);
